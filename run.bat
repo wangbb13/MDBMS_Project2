b22a39e936c02bb5ae6baf90d8d0cab5880b98f2
wangbb13 <18800160527@163.com>
2018-11-28 21:37:20 +0800

    ...

--- run.bat
+++ run.bat
@@ -56,7 +56,7 @@ rem pause
 
 rem ===================  loop version  ===================================
 for %%x in (corr inde anti) do (
-    rem =================== 2 dimensions  ================================
+    echo ================== %%x 2D ================================
     .\main .\datasets\%%x_2.txt .\results\%%x_2_g0.txt 3 0
     .\main .\datasets\%%x_2.txt .\results\%%x_2_g1.txt 3 1
     .\main .\datasets\%%x_2.txt .\results\%%x_2_g2.txt 3 2
@@ -66,9 +66,11 @@ for %%x in (corr inde anti) do (
     python is_same.py .\results\%%x_2_g0.txt .\results\%%x_2_g2.txt
     python is_same.py .\results\%%x_2_g0.txt .\results\%%x_2_g3.txt
     python is_same.py .\results\%%x_2_g0.txt .\results\%%x_2_g4.txt
+    echo ================== %%x 2D END ============================
+    echo.
     
     
-    rem =================== 4 dimensions  ================================
+    echo ================== %%x 4D ================================
     .\main .\datasets\%%x_4.txt .\results\%%x_4_g0.txt 3 0
     .\main .\datasets\%%x_4.txt .\results\%%x_4_g1.txt 3 1
     .\main .\datasets\%%x_4.txt .\results\%%x_4_g2.txt 3 2
@@ -78,9 +80,11 @@ for %%x in (corr inde anti) do (
     python is_same.py .\results\%%x_4_g0.txt .\results\%%x_4_g2.txt
     python is_same.py .\results\%%x_4_g0.txt .\results\%%x_4_g3.txt
     python is_same.py .\results\%%x_4_g0.txt .\results\%%x_4_g4.txt
+    echo ================== %%x 4D END ============================
+    echo.
     
     
-    rem =================== 6 dimensions  ================================
+    echo ================== %%x 6D ================================
     rem .\main .\datasets\%%x_6.txt .\results\%%x_6_g0.txt 3 0
     .\main .\datasets\%%x_6.txt .\results\%%x_6_g1.txt 3 1
     .\main .\datasets\%%x_6.txt .\results\%%x_6_g2.txt 3 2
@@ -93,21 +97,25 @@ for %%x in (corr inde anti) do (
     python is_same.py .\results\%%x_6_g1.txt .\results\%%x_6_g2.txt
     python is_same.py .\results\%%x_6_g2.txt .\results\%%x_6_g3.txt
     python is_same.py .\results\%%x_6_g3.txt .\results\%%x_6_g4.txt
+    echo ================== %%x 6D END ============================
+    echo.
     
     
-    rem =================== 8 dimensions  ===================================
+    rem echo ================== %%x 8D ================================
     rem .\main .\datasets\%%x_8.txt .\results\%%x_8_g0.txt 3 0
-    .\main .\datasets\%%x_8.txt .\results\%%x_8_g1.txt 3 1
-    .\main .\datasets\%%x_8.txt .\results\%%x_8_g2.txt 3 2
-    .\main .\datasets\%%x_8.txt .\results\%%x_8_g3.txt 3 3
-    .\main .\datasets\%%x_8.txt .\results\%%x_8_g4.txt 3 4
+    rem .\main .\datasets\%%x_8.txt .\results\%%x_8_g1.txt 3 1
+    rem .\main .\datasets\%%x_8.txt .\results\%%x_8_g2.txt 3 2
+    rem .\main .\datasets\%%x_8.txt .\results\%%x_8_g3.txt 3 3
+    rem .\main .\datasets\%%x_8.txt .\results\%%x_8_g4.txt 3 4
     rem python is_same.py .\results\%%x_8_g0.txt .\results\%%x_8_g1.txt
     rem python is_same.py .\results\%%x_8_g0.txt .\results\%%x_8_g2.txt
     rem python is_same.py .\results\%%x_8_g0.txt .\results\%%x_8_g3.txt
     rem python is_same.py .\results\%%x_8_g0.txt .\results\%%x_8_g4.txt
-    python is_same.py .\results\%%x_8_g1.txt .\results\%%x_8_g2.txt
-    python is_same.py .\results\%%x_8_g2.txt .\results\%%x_8_g3.txt
-    python is_same.py .\results\%%x_8_g3.txt .\results\%%x_8_g4.txt
+    rem python is_same.py .\results\%%x_8_g1.txt .\results\%%x_8_g2.txt
+    rem python is_same.py .\results\%%x_8_g2.txt .\results\%%x_8_g3.txt
+    rem python is_same.py .\results\%%x_8_g3.txt .\results\%%x_8_g4.txt
+    rem echo ================== %%x 8D END ============================
+    rem echo.
 )
 
 pause
